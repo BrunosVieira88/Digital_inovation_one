37562c1f354a42ab94381c6b62b7f8cfd46c3e12
Bruno Santos Vieira <brunosvieira88@gmail.com>
2021-12-29 18:32:25 -0300

    finalizado to do list

--- assets/teste02.js
+++ assets/teste02.js
@@ -1,21 +1,42 @@
-function tarefa(){
-   let item = document.createElement('div');
-   let valor = document.querySelector("#frase"); 
+const formulario = document.getElementById("formulario");
+console.log(formulario);
+const tarefas = document.getElementById("lista_tarefas");
+console.log(tarefas);
 
-   if(valor.value == ""){
-       alert("Você precisa criar uma tarefa!")
-   }else{
-    item.innerHTML = `
-   
-    <input type="checkbox"><label>${valor.value}</label>
-   
-   `;
-    document.querySelector("#aparece").appendChild(item);
-
-
-   }
+formulario.onsubmit = function (e){
+    e.preventDefault();
+    let entrada = document.getElementById("tarefa");
+    console.log(entrada.value);
+    if(entrada.value == ""){
+        alert("Voce nao digitou uma tarefa!");
+    }else{
   
-  
-   
+        addDiv(entrada.value);
+        formulario.reset(); 
+    }
+
+};
+
+function addDiv(description){
+    const taskContainer = document.createElement('div');
+	const newTask = document.createElement('input');
+	const taskLabel = document.createElement('label');
+	const taskDescriptionNode = document.createTextNode(description);
+
+    
+	newTask.setAttribute('type', 'checkbox');
+	newTask.setAttribute('name', description);
+	newTask.setAttribute('id', description);
+    newTask.setAttribute('class','esquerda');
+
+    taskLabel.setAttribute('class','esquerda');
+	taskLabel.setAttribute('for', description);
+	taskLabel.appendChild(taskDescriptionNode);
+
+    taskContainer.classList.add('tamanho');
+	taskContainer.appendChild(newTask);
+	taskContainer.appendChild(taskLabel);
+
+	tarefas.appendChild(taskContainer);
 
 }
